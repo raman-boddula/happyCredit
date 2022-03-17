2ca018ce8a79aa9e4d26e479b84ad51ad5c497b4
Raman Boddula <raman_rc001@yahoo.com>
2022-03-17 15:17:36 +0530

    left with few features

---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import { Footer } from "./Footer";
+import { Homepage } from "./Homepage";
+import { Navbar } from "./Navbar";
+import "./Style.css";
+
+export const Home = () => {
+    return (
+        <>
+        <Navbar />
+        <Homepage />
+        <Footer />
+        </>
+    )
+}
